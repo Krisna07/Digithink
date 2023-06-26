1777d2c7a20dcf4fcbf3a20f1f645a313a96dabd
Krishna <krisnachhetri07@gmail.com>
2023-06-26 11:29:21 +1000

    intersection animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
 import Customers from "./Components/Homepage/Customers";
 import Digital from "./Components/Homepage/Digital";
 import Goal from "./Components/Homepage/Goal";
@@ -7,27 +9,77 @@ import Requestform from "./Components/Homepage/Requestform";
 import Social from "./Components/Homepage/social";
 
 export default function Home() {
+  const [isIntersecting, setIsIntersecting] = useState<boolean>(false);
+  const ref = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      { rootMargin: "-300px" }
+    );
+    console.log(isIntersecting);
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [isIntersecting]);
+
   return (
     <main className="w-full grid-place-items-center">
-      <div className="w-full py-8   p-4 bg-gradient-to-r from-black to-slate-900 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Herosection />
       </div>
-      <div className="w-full py-8   p-4 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Goal />
       </div>
-      <div className="w-full py-8   p-4 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Digital />
       </div>
-      <div className="w-full py-8   p-4 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Homeservices />
       </div>
-      <div className="w-full py-8   p-4 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Social />
       </div>
-      <div className="w-full py-8 skew-y-[-2deg] bg-slate-200  p-4 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Customers />
       </div>
-      <div className="w-full py-8   p-4 grid place-items-center box-border text-center gap-4 relative">
+      <div
+        ref={ref}
+        className={`w-full py-8 ${
+          !isIntersecting ? "opacity-0" : "opcaity-1"
+        }   p-4 grid place-items-center box-border text-center gap-4 relative transition-opacity ease-in-out duration-1000`}
+      >
         <Requestform />
       </div>
     </main>
